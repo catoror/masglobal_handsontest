f47e23aefd59b14e14180406281f6f98cf101722
Carlos <carlos.toro@siata.gov.co>
2021-03-15 03:50:07 -0500

    Se corrige bug de URL problemática

--- masglobal/masglobal/urls.py
+++ masglobal/masglobal/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('api/', include('employees_api.urls')),
     path('view/', include('employees_view.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
